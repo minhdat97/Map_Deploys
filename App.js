1b54bfa862cb8f247e419257e4820689b488301e
TRAN MINH DAT <1512103@student.hcmus.edu.vn>
2019-06-06 19:26:44 +0700

    Add icon user and tracking user location

--- App.js
+++ App.js
@@ -7,7 +7,10 @@ import {
   Dimensions,
   Keyboard,
   Alert,
-  TouchableOpacity
+  TouchableOpacity,
+  AlertIOS,
+  Platform,
+  AlertAndroid
 } from "react-native";
 import {
   Constants,
@@ -15,12 +18,15 @@ import {
   Permissions,
   Location,
   Font,
-  AppLoading
+  AppLoading,
+  Marker
 } from "expo";
 
 // or any pure javascript modules available in npm
 import { Card } from "react-native-paper";
 import { InputGroup, Input, Button, H3, Icon } from "native-base";
+const Scaledrone = require("scaledrone-react-native");
+const SCALEDRONE_CHANNEL_ID = require("./scaledrone_channel_id.json");
 
 // You can import from local files
 import apis from "./components/Constant/api";
@@ -34,7 +40,7 @@ const LONGITUDE = 106.65898310270131;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
-const GOOGLE_MAPS_APIKEY = "AIzaSyDx7Fu77vp5iLD9_Ki9BUEWsmMlBVF_3NQ";
+const GOOGLE_MAPS_APIKEY = "AIzaSyDCLvmZ_auB3d8yaIkHHZMu83Jta4Vir8w";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -66,10 +72,13 @@ export default class App extends React.Component {
       value: null,
       temp_value: null,
       fontLoaded: false,
-      isListingSelected: false
+      isListingSelected: false,
+      members: []
     };
     this._results = null;
     this.mapView = null;
+    this._clientID = null;
+    this.arrayMarker = [];
   }
 
   async componentWillMount() {
@@ -109,50 +118,161 @@ export default class App extends React.Component {
         distanceFilter: 1,
       }
     );*/
-    this._getLocationAsync();
+    //this._getLocationAsync();
+    this._getLocationFriendAsync();
   }
 
-  _getLocationAsync = async () => {
-    // let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    // if (status !== "granted") {
-    //   this.setState({
-    //     locationResult: "Permission to access location was denied"
-    //   });
-    // }
+  _getLocationFriendAsync = async () => {
+    const drone = new Scaledrone(SCALEDRONE_CHANNEL_ID);
+    drone.on("error", error => console.error(error));
+    drone.on("close", reason => console.error(reason));
+    drone.on("open", error => {
+      if (error) {
+        return console.error(error);
+      }
+      if (Platform.OS === "ios") {
+        // AlertIOS.prompt("Please insert your name", null, name =>
+        //   doAuthRequest(drone.clientId, name).then(jwt =>
+        //     drone.authenticate(jwt)
+        //   )
+        // );
+        doAuthRequest(drone.clientId, "Đạt").then(jwt =>
+          drone.authenticate(jwt)
+        );
+      } else {
+        AlertAndroid.prompt("Please insert your name", null, name =>
+          doAuthRequest(drone.clientId, name).then(jwt =>
+            drone.authenticate(jwt)
+          )
+        );
+      }
+      this._clientID = drone.clientId;
 
-    // let location = await Location.getCurrentPositionAsync({});
-    // let region = {
-    //   latitude: location.coords.latitude,
-    //   longitude: location.coords.longitude,
-    //   latitudeDelta: LATITUDE_DELTA,
-    //   longitudeDelta: LONGITUDE_DELTA
-    // };
-    // //this.setState({ locationResult: JSON.stringify(location) });
-    // this.onRegionChange(region, region.latitude, region.longitude);
-
-    // console.log(JSON.stringify(location));
-    this.watchID = navigator.geolocation.watchPosition(
-      position => {
-        // Create the object to update this.state.mapRegion through the onRegionChange function
+      //doAuthRequest(drone.clientId, "Tôi").then(jwt => drone.authenticate(jwt));
+    });
+    const room = drone.subscribe("observable-locations", {
+      historyCount: 50 // load 50 past messages
+    });
+    room.on("open", error => {
+      if (error) {
+        return console.error(error);
+      }
+      this.startLocationTracking(position => {
+        const { latitude, longitude } = position.coords;
         let region = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
+          latitude: latitude,
+          longitude: longitude,
           latitudeDelta: LATITUDE_DELTA,
           longitudeDelta: LONGITUDE_DELTA
         };
-        console.log("here");
-        console.log("lat", position.coords.latitude);
         this.onRegionChange(region, region.latitude, region.longitude);
-      },
-      error => {
-        this.setState({ error: error.message });
-      },
-      {
-        enableHighAccuracy: true,
-        distanceFilter: 1
-      }
+        // publish device's new location
+        drone.publish({
+          room: "observable-locations",
+          message: { latitude, longitude }
+        });
+      });
+    });
+    // received past message
+    room.on("history_message", message =>
+      this.updateLocation(message.data, message.clientId)
     );
+    // received new message
+    room.on("data", (data, member) => this.updateLocation(data, member.id));
+    // array of all connected members
+    room.on("members", members => this.setState({ members }));
+    // new member joined room
+    room.on("member_join", member => {
+      const members = this.state.members.slice(0);
+      members.push(member);
+      this.setState({ members });
+    });
+    // member left room
+    room.on("member_leave", member => {
+      const members = this.state.members.slice(0);
+      const index = members.findIndex(m => m.id === member.id);
+      if (index !== -1) {
+        members.splice(index, 1);
+        this.setState({ members });
+      }
+    });
   };
+
+  startLocationTracking(callback) {
+    navigator.geolocation.watchPosition(callback, error => console.log(error), {
+      enableHighAccuracy: true,
+      timeout: 20000,
+      maximumAge: 1000,
+      distanceFilter: 1
+    });
+  }
+
+  updateLocation(data, memberId) {
+    const { members } = this.state;
+    const member = members.find(m => m.id === memberId);
+    if (!member) {
+      // a history message might be sent from a user who is no longer online
+      return;
+    }
+    if (member.location) {
+      member.location
+        .timing({
+          latitude: data.latitude,
+          longitude: data.longitude
+        })
+        .start();
+    } else {
+      member.location = new MapView.AnimatedRegion({
+        latitude: data.latitude,
+        longitude: data.longitude,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      });
+      this.forceUpdate();
+    }
+  }
+
+  // _getLocationAsync = async () => {
+  //   // let { status } = await Permissions.askAsync(Permissions.LOCATION);
+  //   // if (status !== "granted") {
+  //   //   this.setState({
+  //   //     locationResult: "Permission to access location was denied"
+  //   //   });
+  //   // }
+
+  //   // let location = await Location.getCurrentPositionAsync({});
+  //   // let region = {
+  //   //   latitude: location.coords.latitude,
+  //   //   longitude: location.coords.longitude,
+  //   //   latitudeDelta: LATITUDE_DELTA,
+  //   //   longitudeDelta: LONGITUDE_DELTA
+  //   // };
+  //   // //this.setState({ locationResult: JSON.stringify(location) });
+  //   // this.onRegionChange(region, region.latitude, region.longitude);
+
+  //   // console.log(JSON.stringify(location));
+  //   this.watchID = navigator.geolocation.watchPosition(
+  //     position => {
+  //       // Create the object to update this.state.mapRegion through the onRegionChange function
+  //       let region = {
+  //         latitude: position.coords.latitude,
+  //         longitude: position.coords.longitude,
+  //         latitudeDelta: LATITUDE_DELTA,
+  //         longitudeDelta: LONGITUDE_DELTA
+  //       };
+  //       console.log("here");
+  //       console.log("lat", position.coords.latitude);
+  //       this.onRegionChange(region, region.latitude, region.longitude);
+  //     },
+  //     error => {
+  //       this.setState({ error: error.message });
+  //     },
+  //     {
+  //       enableHighAccuracy: true,
+  //       distanceFilter: 1
+  //     }
+  //   );
+  // };
 
   onRegionChange(region, lastLat, lastLong) {
     this.setState({
@@ -165,6 +285,7 @@ export default class App extends React.Component {
       //lastLat: lastLat || this.state.lastLat,
       //lastLong: lastLong || this.state.lastLong,
     });
+    this.arrayMarker.push(this.state.curCoordinates);
     console.log(this.state.mapRegion);
   }
 
@@ -186,6 +307,7 @@ export default class App extends React.Component {
             this.setState({
               destCoordinates: this._results.geometry.location
             });
+            this.arrayMarker.push(this.state.destCoordinates);
             console.log("destCoordinates", this.state.destCoordinates);
           } else {
             return console.log("error");
@@ -213,17 +335,21 @@ export default class App extends React.Component {
 
   containsObject = (obj, list) => {
     let i;
-    for (i = 0; i < list.length; i++) {
-      if (list.hasOwnProperty(x) && list[i] === obj) {
-        return true;
-      }
-    }
-
-    return false;
+    let found = list.find(
+      el =>
+        (el.lat === obj.latitude && el.lng === obj.longitude) ||
+        (el.latitude === obj.latitude && el.longitude === obj.longitude)
+    );
+    if (!!found) return true;
+    else return false;
   };
 
   onMapPress = e => {
-    console.log(e.nativeEvent.coordinate);
+    console.log("e.nativeEvent.coordinate", e.nativeEvent.coordinate);
+    console.log("this.arrayMarker", this.arrayMarker);
+    console.log(
+      this.containsObject(e.nativeEvent.coordinate, this.arrayMarker)
+    );
     /*if (this.state.coordinates.length == 2) {
       this.setState({
         coordinates: [e.nativeEvent.coordinate],
@@ -233,18 +359,13 @@ export default class App extends React.Component {
         coordinates: [...this.state.coordinates, e.nativeEvent.coordinate],
       });
     }*/
-    if (
-      this.containsObject(
-        e.nativeEvent.coordinate,
-        this.state.destCoordinates
-      ) ||
-      this.containsObject(e.nativeEvent.coordinate, this.state.coordinates)
-    ) {
+    if (this.containsObject(e.nativeEvent.coordinate, this.arrayMarker)) {
       return null;
     } else {
       this.setState({
         coordinates: [...this.state.coordinates, e.nativeEvent.coordinate]
       });
+      this.arrayMarker.push(this.state.coordinates);
     }
 
     console.log(this.state.coordinates);
@@ -285,6 +406,7 @@ export default class App extends React.Component {
           showsPointsOfInterest={true}
           onPress={this.onMapPress.bind(this)}
         >
+          {this.createMarkers()}
           {!!this.state.destCoordinates.lat && (
             <MapView.Marker
               coordinate={{
@@ -356,7 +478,7 @@ export default class App extends React.Component {
             </InputGroup>
           </View>
         </View>
-        <View style={styles.buttonContainer}>
+        {/* <View style={styles.buttonContainer}>
           {!this.state.isListingSelected && (
             <TouchableOpacity
               style={styles.myLocationButton}
@@ -367,10 +489,91 @@ export default class App extends React.Component {
               <Icon name="md-navigate" size={24} />
             </TouchableOpacity>
           )}
+        </View> */}
+        <View pointerEvents="none" style={styles.members}>
+          {this.createMembers()}
+        </View>
+        <View style={styles.buttonFitContainer}>
+          <TouchableOpacity
+            onPress={() => this.fitToMarkersToMap()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text>Fit Markers Onto Map</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
   }
+
+  createMarkers() {
+    const { members } = this.state;
+
+    const membersWithLocations = members.filter(m => !!m.location);
+    return membersWithLocations.map(member => {
+      const { id, location, authData } = member;
+      const { name, color } = authData;
+
+      return (
+        <MapView.Marker.Animated
+          key={id}
+          identifier={id}
+          coordinate={location}
+          pinColor={color}
+          title={name}
+        />
+      );
+    });
+  }
+
+  createMembers() {
+    const { members } = this.state;
+
+    const memberMe = members.find(m => m.id === this._clientID);
+    return members.map(member => {
+      const { name, color } = member.authData;
+      return (
+        <View key={member.id} style={styles.member}>
+          <View style={[styles.avatar, { backgroundColor: color }]} />
+          {member.id === this._clientID ? (
+            <Text style={styles.memberName}>Tôi</Text>
+          ) : (
+            <Text style={styles.memberName}>{name}</Text>
+          )}
+        </View>
+      );
+    });
+  }
+
+  fitToMarkersToMap() {
+    const { members } = this.state;
+    this.mapView.fitToSuppliedMarkers(members.map(m => m.id), true);
+  }
+}
+
+function doAuthRequest(clientId, name) {
+  let status;
+  console.log("go here");
+  return fetch("http://192.168.0.107:3000/auth", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ clientId, name })
+  })
+    .then(res => {
+      status = res.status;
+      console.log("res", res);
+      return res.text();
+    })
+    .then(text => {
+      if (status === 200) {
+        return text;
+      } else {
+        alert(text);
+      }
+    })
+    .catch(error => console.error(error));
 }
 
 const styles = StyleSheet.create({
@@ -436,6 +639,57 @@ const styles = StyleSheet.create({
     //padding: 15,
     //elevation: 3,
     alignSelf: "flex-end"
+  },
+  bubble: {
+    flex: 1,
+    backgroundColor: "rgba(255,255,255,0.7)",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+    marginRight: 20
+  },
+  latlng: {
+    width: 200,
+    alignItems: "stretch"
+  },
+  button: {
+    paddingHorizontal: 12,
+    alignItems: "center",
+    marginHorizontal: 10
+  },
+  buttonFitContainer: {
+    width: Dimensions.get("window").width,
+    position: "absolute",
+    bottom: 0,
+    flexDirection: "row",
+    marginVertical: 16,
+    backgroundColor: "transparent"
+  },
+  members: {
+    position: "absolute",
+    bottom: 80,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    width: "100%",
+    paddingHorizontal: 10
+  },
+  member: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(255,255,255,1)",
+    borderRadius: 20,
+    height: 30,
+    marginTop: 10
+  },
+  memberName: {
+    marginHorizontal: 10
+  },
+  avatar: {
+    height: 30,
+    width: 30,
+    borderRadius: 15
   }
 });
 
